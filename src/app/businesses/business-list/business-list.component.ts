86b24b9021466d892207d69915a23605ad5c8ab6
1lee <1lee.burrell@gmail.com>
2022-11-13 16:55:39 -0500

    markup for business detail component

--- src/app/businesses/business-list/business-list.component.ts
+++ src/app/businesses/business-list/business-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Business } from '../business.model';
 
 @Component({
   selector: 'app-business-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./business-list.component.css']
 })
 export class BusinessListComponent implements OnInit {
+
+  businesses: Business[] = [
+    new Business('A Test Business', 'A test description for a fake business', 'https://hbr.org/resources/images/article_assets/2022/08/Hero-Image-1024x576.png'),
+    new Business('A Test Business', 'A test description for a fake business', 'https://hbr.org/resources/images/article_assets/2022/08/Hero-Image-1024x576.png')
+
+  ];
 
   constructor() { }
 
