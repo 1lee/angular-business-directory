4cae87eb6f484bcb78158913566c972deb9f35da
1lee <1lee.burrell@gmail.com>
2022-11-13 17:58:45 -0500

    basic setup

--- src/app/link-list/link-list.component.ts
+++ src/app/link-list/link-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Link } from '../shared/link.model';
 
 @Component({
   selector: 'app-link-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./link-list.component.css']
 })
 export class LinkListComponent implements OnInit {
+  links: Link[] = [
+    new Link('Google search', 'http://www.google.com'),
+    new Link('Amazon', 'http://www.amazon.com')
+  ];
 
   constructor() { }
 
