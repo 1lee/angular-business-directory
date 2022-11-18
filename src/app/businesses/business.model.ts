02b8b28fb79592d32d7059f0266422e476e71517
1lee <1lee.burrell@gmail.com>
2022-11-17 22:40:44 -0500

    checkpoint

--- src/app/businesses/business.model.ts
+++ src/app/businesses/business.model.ts
@@ -1,11 +1,15 @@
+import { Link } from '../shared/link.model'
+
 export class Business {
   public name: string;
   public description: string;
   public imagePath: string;
+  public links: Link[];
 
-  constructor(name: string, desc: string, imagePath: string) {
+  constructor(name: string, desc: string, imagePath: string, links: Link[]) {
     this.name = name;
     this.description = desc;
     this.imagePath = imagePath;
+    this.links = links;
   }
 }
--- src/app/businesses/business.service.ts
+++ src/app/businesses/business.service.ts
@@ -1,12 +1,18 @@
 import { EventEmitter } from '@angular/core'
+import { Link } from '../shared/link.model';
 import { Business } from './business.model'
 
 export class BusinessService {
   businessSelected = new EventEmitter<Business>()
 
   private businesses: Business[] = [
-    new Business('A Test Business', 'A test description for a fake business', 'https://hbr.org/resources/images/article_assets/2022/08/Hero-Image-1024x576.png'),
-    new Business('Another Test Business', 'Another test description for a fake business', 'https://hbr.org/resources/images/article_assets/2022/08/Hero-Image-1024x576.png')
+    new Business('A Test Business',
+      'A test description for a fake business',
+      'https://hbr.org/resources/images/article_assets/2022/08/Hero-Image-1024x576.png',
+      [new Link('Funk Biscuit Co.', 'www.funkbiscuitcoltd.whiskers'), new Link('Neberwordies Thiscklist', 'www.Neberwordiesusa.org')]),
+    new Business('Another Test Business',
+      'Another test description for a fake business',
+      'https://hbr.org/resources/images/article_assets/2022/08/Hero-Image-1024x576.png', [new Link('Sugar Pork Gummy Alliance', 'www.getsugargummiedtoday.grease')])
 
   ];
 
