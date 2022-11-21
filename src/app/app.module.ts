e166e1070408c02ddfb3de696bbb665c3f9f9123
1lee <1lee.burrell@gmail.com>
2022-11-21 15:49:17 -0500

    fixed FormArray issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -36,7 +36,8 @@ import { BusinessEditComponent } from './businesses/business-edit/business-edit.
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [LinkListService],
   bootstrap: [AppComponent]
--- src/app/businesses/business-edit/business-edit.component.ts
+++ src/app/businesses/business-edit/business-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { BusinessService } from '../business.service';
 
 @Component({
   selector: 'app-business-edit',
@@ -9,8 +11,9 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class BusinessEditComponent implements OnInit {
   id: number;
   editMode: boolean = false;
+  businessForm: FormGroup;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private businessService: BusinessService) {
 
   }
 
@@ -18,7 +21,48 @@ export class BusinessEditComponent implements OnInit {
     this.route.params.subscribe((params: Params) => {
       this.id = +params['id'];
       this.editMode = params['id'] != null;
+      this.initForm();
     })
+  }
+
+  get controls() { // a getter!
+    return (<FormArray>this.businessForm.get('links')).controls;
+  }
+
+  private initForm() {
+    let businessName = '';
+    let businessImagePath = '';
+    let businessDescription = '';
+    let businessLinks = new FormArray([]);
+
+    if (this.editMode) {
+      const business = this.businessService.getBusiness(this.id);
+      console.log(`business: `, business)
+      businessName = business.name;
+      businessImagePath = business.imagePath;
+      businessDescription = business.description;
+
+      if (business['links']) {
+        for (let link of business.links) {
+          businessLinks.push(new FormGroup({
+            'name': new FormControl(link.name),
+            'url': new FormControl(link.url)
+          }))
+        }
+      }
+    }
+
+    this.businessForm = new FormGroup({
+      'name': new FormControl(businessName),
+      'imagePath': new FormControl(businessImagePath),
+      'description': new FormControl(businessDescription),
+      'links': businessLinks
+    })
+    console.log(`businessForm: `, this.businessForm)
+  }
+
+  onSubmit() {
+    console.log(this.businessForm);
   }
 
 }
